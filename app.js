025302bb1c337c18ff45f077971a78e735853fa9
Nasnuven <101296266+nasnuven@users.noreply.github.com>
2022-03-31 18:28:28 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,137 @@
+var listaTripKit = [
+    "https://www.bhphotovideo.com/cdn-cgi/image/format=auto,fit=scale-down,width=500,quality=95/https://www.bhphotovideo.com/images/images500x500/zoom_zf3_f3_digital_field_recorder_1642674016_1686005.jpg",
+    "https://www.bhphotovideo.com/cdn-cgi/image/format=auto,fit=scale-down,width=500,quality=95/https://www.bhphotovideo.com/images/images500x500/zoom_iq7_ms_stereo_mic_1414678814_1090754.jpg",
+  
+    "https://www.bhphotovideo.com/cdn-cgi/image/format=auto,fit=scale-down,width=500,quality=95/https://www.bhphotovideo.com/images/images500x500/Pro_Co_Sound_LMX_LMX_In_Line_Barrel_XLR_1375110688_231520.jpg"
+  ];
+  var linkTripKit = [
+    "https://www.bhphotovideo.com/c/product/1686005-REG/zoom_zf3_f3_digital_field_recorder.html",
+    "https://www.bhphotovideo.com/c/product/1090754-REG/zoom_iq7_ms_stereo_mic.html",
+    "https://www.bhphotovideo.com/c/product/231520-REG/Pro_Co_Sound_LMX_LMX_In_Line_Barrel_XLR.html"
+  ];
+  
+  // Gerando todas as imagens pré-definidas
+  for (var i = 0; i < listaTripKit.length; i++) {
+    const tripKit = document.getElementById("listaTripKit");
+    tripKit.innerHTML +=
+      "<a href='" +
+      linkTripKit[i] +
+      "' target='_blank' rel='noopener noreferrer'>" +
+      '<img class="capas" src=' +
+      listaTripKit[i] +
+      ">" +
+      "</a>";
+  }
+  
+  var listaTripKit2 = [
+    "https://www.bhphotovideo.com/cdn-cgi/image/format=auto,fit=scale-down,width=500,quality=95/https://www.bhphotovideo.com/images/images500x500/zoom_zf1lp_zoom_f1_field_recorder_1516751191_1385361.jpg",
+  
+    "https://www.bhphotovideo.com/cdn-cgi/image/format=auto,fit=scale-down,width=500,quality=95/https://www.bhphotovideo.com/images/images500x500/zoom_exh_6_dual_xlr_trs_input_capsule_1370522876_981648.jpg",
+    "https://www.bhphotovideo.com/cdn-cgi/image/format=auto,fit=scale-down,width=500,quality=95/https://www.bhphotovideo.com/images/images500x500/zoom_xyh_5_mic_capsule_shockmount_for_zoom_1409135702_1078028.jpg",
+    "https://www.bhphotovideo.com/cdn-cgi/image/format=auto,fit=scale-down,width=500,quality=95/https://www.bhphotovideo.com/images/images500x500/zoom_zxyh6_xyh_6_x_y_capsule_1407335816_1071896.jpg",
+    "https://www.bhphotovideo.com/cdn-cgi/image/format=auto,fit=scale-down,width=500,quality=95/https://www.bhphotovideo.com/images/images500x500/zoom_zmsh6_msh_6_mid_side_microphone_1407335816_1071897.jpg",
+    "https://www.bhphotovideo.com/cdn-cgi/image/format=auto,fit=scale-down,width=500,quality=95/https://www.bhphotovideo.com/images/images500x500/zoom_zsmf1_shock_mount_for_f1_1533835265_1428726.jpg",
+  
+    "https://static.bhphoto.com/images/images500x500/audio_technica_ath_m30x_closed_back_professional_studio_1415893536_1024220.jpg",
+    "https://static.bhphoto.com/images/multiple_images/images500x500/1317204064_IMG_222947.jpg"
+  ];
+  var linkTripKit2 = [
+    "https://www.bhphotovideo.com/c/product/1385361-REG/zoom_zf1lp_zoom_f1_field_recorder.html/accessories",
+  
+    "https://www.bhphotovideo.com/c/product/981648-REG/zoom_exh_6_dual_xlr_trs_input_capsule.html?sts=pi&pim=Y",
+    "https://www.bhphotovideo.com/c/product/1078028-REG/zoom_xyh_5_mic_capsule_shockmount_for_zoom.html?sts=pi&pim=Y",
+    "https://www.bhphotovideo.com/c/product/1071896-REG/zoom_zxyh6_xyh_6_x_y_capsule.html?sts=pi&pim=Y",
+    "https://www.bhphotovideo.com/c/product/1071897-REG/zoom_zmsh6_msh_6_mid_side_microphone.html?sts=pi&pim=Y",
+    "https://www.bhphotovideo.com/c/product/1428726-REG/zoom_zsmf1_shock_mount_for_f1.html",
+  
+    "https://www.bhphotovideo.com/c/product/1416309-REG/audio_technica_ath_m30x_monitor_headphones_and.html",
+    "https://www.bhphotovideo.com/c/product/49510-REG/Sony_MDR_7506_MDR_7506_Headphone.html"
+  ];
+  
+  // Gerando todas as imagens pré-definidas
+  for (var i = 0; i < listaTripKit2.length; i++) {
+    const tripKit2 = document.getElementById("listaTripKit2");
+    tripKit2.innerHTML +=
+      "<a href='" +
+      linkTripKit2[i] +
+      "' target='_blank' rel='noopener noreferrer'>" +
+      '<img class="capas" src=' +
+      listaTripKit2[i] +
+      ">" +
+      "</a>";
+  }
+  var listaBoom = [
+    "https://www.bhphotovideo.com/cdn-cgi/image/format=auto,fit=scale-down,width=500,quality=95/https://www.bhphotovideo.com/images/images500x500/Sennheiser_MKE_600_Shotgun_1616159147_878340.jpg",
+  
+    "https://www.bhphotovideo.com/cdn-cgi/image/format=auto,fit=scale-down,width=500,quality=95/https://www.bhphotovideo.com/images/images500x500/auray_bp_47a_4_section_aluminum_boom_pole_1638873343_1606021.jpg",
+    "https://www.bhphotovideo.com/cdn-cgi/image/format=auto,fit=scale-down,width=500,quality=95/https://www.bhphotovideo.com/images/images500x500/auray_dusm_1_universal_shock_mount_1612892803_885660.jpg",
+    "https://yt3.ggpht.com/kS4t4QRwLD-q1BhhiRVzVzQimW65zDkopFa8dD14i56oc8ii0G253j6RqC3HxcZ-ytzDhUV-=s88-c-k-c0x00ffffff-no-rj",
+    "https://static.bhphoto.com/images/multiple_images/images500x500/1379333153_IMG_338503.jpg",
+    "https://static.bhphoto.com/images/multiple_images/images500x500/1620038880_IMG_1527903.jpg"
+  ];
+  var linkBoom = [
+    "https://www.bhphotovideo.com/c/product/878340-REG/Sennheiser_MKE_600_Shotgun.html",
+    "https://www.bhphotovideo.com/c/product/1606021-REG/auray_bp_47a_4_section_aluminum_boom_pole.html",
+    "https://www.bhphotovideo.com/c/product/885660-REG/auray_dusm_1_universal_shock_mount.html",
+    "https://www.bhphotovideo.com/c/search?q=shockmount&filters=fct_price%3A20..100",
+    "https://www.bhphotovideo.com/c/product/850315-REG/auray_wss_2014_pro_matrixwindsheild_20mm_di.html",
+    "https://www.bhphotovideo.com/c/product/1632615-REG/boya_by_ws1000_professional_blimp_windshield_and.html"
+  ];
+  
+  // Gerando todas as imagens pré-definidas
+  for (var i = 0; i < listaBoom.length; i++) {
+    const boom = document.getElementById("listaBoom");
+    boom.innerHTML +=
+      "<a href='" +
+      linkBoom[i] +
+      "' target='_blank' rel='noopener noreferrer'>" +
+      '<img class="capas" src=' +
+      listaBoom[i] +
+      ">" +
+      "</a>";
+  }
+  
+  var listaExtra = [
+    "https://www.bhphotovideo.com/cdn-cgi/image/format=auto,fit=scale-down,width=500,quality=95/https://www.bhphotovideo.com/images/images500x500/zoom_zh6ab_h6_all_black_6_input_1585730760_1554843.jpg",
+  
+    "https://www.bhphotovideo.com/cdn-cgi/image/format=auto,fit=scale-down,width=500,quality=95/https://www.bhphotovideo.com/images/images500x500/rode_ntg5_broadcast_shotgun_mic_1571845320_1512606.jpg",
+  
+    "https://www.bhphotovideo.com/cdn-cgi/image/format=auto,fit=scale-down,width=500,quality=95/https://www.bhphotovideo.com/images/images500x500/shure_mv88_a_motiv_mv88_digital_stereo_1463052908_1251142.jpg",
+    "https://www.bhphotovideo.com/cdn-cgi/image/format=auto,fit=scale-down,width=500,quality=95/https://www.bhphotovideo.com/images/images500x500/zoom_zh1nr_h1n_digital_handy_stereo_1588776684_1560459.jpg",
+    "https://www.bhphotovideo.com/cdn-cgi/image/format=auto,fit=scale-down,width=500,quality=95/https://www.bhphotovideo.com/images/images500x500/sjcam_f1r_f1_funcam_sports_video_1616407524_1629159.jpg",
+    "https://www.bhphotovideo.com/cdn-cgi/image/format=auto,fit=scale-down,width=500,quality=95/https://www.bhphotovideo.com/images/images500x500/sjcam_sj8plus_sj8_plus_action_camera_1542105928_1437111.jpg",
+    "https://www.bhphotovideo.com/cdn-cgi/image/format=auto,fit=scale-down,width=500,quality=95/https://www.bhphotovideo.com/images/images500x500/national_geographic_ng_il_2350_icelenad_2ni_camera_hip_1615938458_1629848.jpg",
+    "https://www.bhphotovideo.com/cdn-cgi/image/format=auto,fit=scale-down,width=500,quality=95/https://www.bhphotovideo.com/images/images500x500/sabrent_cr_bca2_usb_3_0_and_usb_1575568249_1514535.jpg",
+    "https://www.bhphotovideo.com/cdn-cgi/image/format=auto,fit=scale-down,width=500,quality=95/https://www.bhphotovideo.com/images/images500x500/sony_mrw_s1_sd_uhs_ii_usb_reader_writer_1494850959_1337340.jpg",
+    "https://static.bhphoto.com/images/multiple_images/images500x500/1504089056_IMG_860215.jpg"
+  ];
+  var linkExtra = [
+    "https://www.bhphotovideo.com/c/product/1554843-REG/zoom_zh6ab_h6_all_black_6_input.html",
+  
+    "https://www.bhphotovideo.com/c/product/1512606-REG/rode_ntg5_broadcast_shotgun_mic.html",
+  
+    "https://www.bhphotovideo.com/c/product/1251142-REG/shure_mv88_a_motiv_mv88_digital_stereo.html",
+    "https://www.bhphotovideo.com/c/product/1560459-REG/zoom_zh1nr_h1n_digital_handy_stereo.html",
+    "https://www.bhphotovideo.com/c/product/1629159-REG/sjcam_f1r_f1_funcam_sports_video.html",
+    "https://www.bhphotovideo.com/c/product/1437111-REG/sjcam_sj8plus_sj8_plus_action_camera.html",
+    "https://www.bhphotovideo.com/c/buy/Belt-Hip-Packs/ci/177/N/4042754127",
+    "https://www.bhphotovideo.com/c/product/1514535-REG/sabrent_cr_bca2_usb_3_0_and_usb.html",
+    "https://www.bhphotovideo.com/c/product/1337340-REG/sony_mrw_s1_sd_uhs_ii_usb_reader_writer.html",
+    "https://www.bhphotovideo.com/c/product/1358231-REG/rode_vxlr_mini_jack_to_xlr_adaptor.html"
+  ];
+  
+  // Gerando todas as imagens pré-definidas
+  for (var i = 0; i < listaExtra.length; i++) {
+    const extra = document.getElementById("listaExtra");
+    extra.innerHTML +=
+      "<a href='" +
+      linkExtra[i] +
+      "' target='_blank' rel='noopener noreferrer'>" +
+      '<img class="capas" src=' +
+      listaExtra[i] +
+      ">" +
+      "</a>";
+  }
+
+  
+  
